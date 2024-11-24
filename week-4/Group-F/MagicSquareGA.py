2963639e9724b5822bcdfaf1afcac2c6ab0d3331
Halgord18 <halgordmuhammad1231@gmail.com>
2024-11-24 21:27:10 +0300

    magic Square complete code.

---
+++ week-4/Group-F/MagicSquareGA.py
@@ -0,0 +1,199 @@
+import random
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+# Define the Magic Square Genetic Algorithm class
+class MagicSquareGA:
+    def __init__(self, population_size=100, generations=1000, mutation_rate=0.1):
+        self.population_size = population_size
+        self.generations = generations
+        self.mutation_rate = mutation_rate
+        self.magic_constant = 15  # For 3x3 magic square with numbers 1-9
+        self.numbers = list(range(1, 10))
+        self.population = []
+        self.best_solutions = []
+
+    def create_individual(self):
+        individual = self.numbers[:]
+        random.shuffle(individual)
+        return individual
+
+    def create_population(self):
+        self.population = [self.create_individual() for _ in range(self.population_size)]
+
+    def fitness(self, individual):
+        rows = [individual[0:3], individual[3:6], individual[6:9]]
+        cols = [individual[0:9:3], individual[1:9:3], individual[2:9:3]]
+        diags = [individual[0:9:4], individual[2:7:2]]
+
+        total_diff = 0
+
+        for line in rows + cols + diags:
+            total_diff += abs(sum(line) - self.magic_constant)
+
+        return total_diff
+
+    def selection(self):
+        # Tournament selection
+        tournament_size = 5
+        selected = []
+        for _ in range(self.population_size):
+            contestants = random.sample(self.population, tournament_size)
+            contestants.sort(key=lambda x: self.fitness(x))
+            selected.append(contestants[0])
+        return selected
+
+    def crossover(self, parent1, parent2):
+        # Order One Crossover (OX1)
+        size = len(parent1)
+        child = [None]*size
+
+        start, end = sorted(random.sample(range(size), 2))
+        child[start:end] = parent1[start:end]
+
+        fill_values = [item for item in parent2 if item not in child]
+        pointer = 0
+        for i in range(size):
+            if child[i] is None:
+                child[i] = fill_values[pointer]
+                pointer +=1
+
+        return child
+
+    def mutate(self, individual):
+        if random.random() < self.mutation_rate:
+            idx1, idx2 = random.sample(range(len(individual)), 2)
+            individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
+        return individual
+
+    def evolve(self):
+        self.create_population()
+
+        for generation in range(self.generations):
+            # Evaluate fitness and find the best individual
+            fitness_scores = [(self.fitness(individual), individual) for individual in self.population]
+            fitness_scores.sort(key=lambda x: x[0])
+            best_fitness, best_individual = fitness_scores[0]
+            self.best_solutions.append({'generation': generation, 'fitness': best_fitness, 'individual': best_individual})
+
+            if best_fitness == 0:
+                print(f"Magic Square found at generation {generation}")
+                break
+
+            # Selection
+            selected = self.selection()
+
+            # Crossover and Mutation
+            next_generation = []
+            for i in range(0, self.population_size, 2):
+                parent1 = selected[i]
+                parent2 = selected[i+1] if i+1 < self.population_size else selected[0]
+                child1 = self.crossover(parent1, parent2)
+                child2 = self.crossover(parent2, parent1)
+                child1 = self.mutate(child1)
+                child2 = self.mutate(child2)
+                next_generation.extend([child1, child2])
+
+            self.population = next_generation[:self.population_size]
+
+class MagicSquareGUI:
+    def __init__(self, master, ga):
+        self.master = master
+        self.ga = ga
+        self.master.title("Magic Square Puzzle")
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Main frame
+        main_frame = ttk.Frame(self.master, padding="10 10 10 10")
+        main_frame.grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+
+        # Run Evolution Button
+        self.run_button = ttk.Button(main_frame, text="Run Genetic Algorithm", command=self.run_ga)
+        self.run_button.grid(row=0, column=0, columnspan=2, pady=10)
+
+        # Reset Button
+        self.reset_button = ttk.Button(main_frame, text="Reset", command=self.reset)
+        self.reset_button.grid(row=2, column=0, columnspan=2, pady=10)
+
+        # Listbox and Scrollbar to display generations
+        self.listbox = tk.Listbox(main_frame, width=40, height=15)
+        self.listbox.grid(row=1, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        scrollbar = ttk.Scrollbar(main_frame, orient=tk.VERTICAL, command=self.listbox.yview)
+        scrollbar.grid(row=1, column=1, sticky=(tk.N, tk.S))
+        self.listbox.configure(yscrollcommand=scrollbar.set)
+        self.listbox.bind('<<ListboxSelect>>', self.on_select)
+
+        # Frame for Magic Square Grid
+        self.grid_frame = ttk.LabelFrame(main_frame, text="Magic Square", padding="10 10 10 10")
+        self.grid_frame.grid(row=0, column=2, rowspan=2, padx=20, sticky=(tk.N))
+
+        # Create grid labels
+        self.grid_labels = []
+        for row in range(3):
+            row_labels = []
+            for col in range(3):
+                label = ttk.Label(self.grid_frame, text="", borderwidth=2, relief="ridge", width=5, anchor="center", background="white", font=("Helvetica", 18))
+                label.grid(row=row, column=col, padx=5, pady=5)
+                row_labels.append(label)
+            self.grid_labels.append(row_labels)
+
+        # Resize behavior
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.rowconfigure(1, weight=1)
+
+    def run_ga(self):
+        # Disable the button to prevent multiple clicks
+        self.run_button.config(state=tk.DISABLED)
+        self.ga.evolve()
+        self.listbox.delete(0, tk.END)
+        for solution in self.ga.best_solutions:
+            self.listbox.insert(tk.END, f"Generation {solution['generation']}, Fitness: {solution['fitness']}")
+        self.run_button.config(state=tk.NORMAL)
+        messagebox.showinfo("Info", "Genetic Algorithm has completed running.")
+
+    def reset(self):
+        # Clear the population and best solutions
+        self.ga.population = []
+        self.ga.best_solutions = []
+
+        # Clear the listbox
+        self.listbox.delete(0, tk.END)
+
+        # Clear the magic square grid
+        for row_labels in self.grid_labels:
+            for label in row_labels:
+                label['text'] = ""
+
+        # Enable the Run button in case it was disabled
+        self.run_button.config(state=tk.NORMAL)
+
+        # Optionally show a message
+        messagebox.showinfo("Info", "The Genetic Algorithm has been reset.")
+
+    def on_select(self, event):
+        if not self.listbox.curselection():
+            return
+        index = self.listbox.curselection()[0]
+        solution = self.ga.best_solutions[index]['individual']
+        self.display_solution(solution)
+
+    def display_solution(self, solution):
+        for i in range(9):
+            row = i // 3
+            col = i % 3
+            self.grid_labels[row][col]['text'] = solution[i]
+
+
+
+
+
+# Main execution
+if __name__ == "__main__":
+    ga = MagicSquareGA()
+    root = tk.Tk()
+    app = MagicSquareGUI(root, ga)
+    root.mainloop()
---
+++ week-4/Group-F/Readme.md
@@ -0,0 +1,105 @@
+# 1. Sudoku 9\*9
+
+Try solving a 9x9 Sudoku puzzle using genetic algorithm.
+
+User can change the initial 9\*9 Sudoku board which is set by you ( you can use some template from internet as initial board) ( initial board is not the goal board, its only a board with some cells predefined and can be changed)
+
+## Requirements:
+
+- main logic must be in Python
+- try solving a 9\*9 sudoku game
+- some of the cells are given and cant be changed ( initial board )
+- the app should show each generations best solution with its fitness function result and give ability for the user to select them from a list and see them
+- genetic algorithm can have the following ( Chromosome representation , Fitness Function , Mutation +any thing else mentioned in the lecture)
+
+# 2. Jigsaw 20\*20
+
+Try solving a 20\*20 jigsaw puzzle using genetic algorithm.
+app should accept an image then split it to 400 pieces and place them randomly
+
+then try to solve it
+
+## Requirements:
+
+- main logic must be in Python
+- accept an image and spread it randomly on a 20\*20 board ( or any UI )
+- solve the Jigsaw game
+- the app should save each generations best solution with its fitness function result and user should have the ability to see them from a list and display them on the board.
+- genetic algorithm can have the following ( Chromosome representation , Fitness Function , Mutation +any thing else mentioned in the lecture)
+
+# 3. N-Bishop puzzle
+
+Use a genetic algorithm to place as many bishops as possible on a chessboard without any bishop threatening another.
+
+Display the solutions on a chess board, and give the ability to show drafted solutions also
+
+## Requirements:
+
+- main logic must be in Python
+- the app should save each generations best solution with its fitness function result and user should have the ability to see them from a list and display them on the board.
+- genetic algorithm can have the following ( Chromosome representation , Fitness Function , Mutation +any thing else mentioned in the lecture)
+
+# 4. PSO Visualization ( Simulation )
+
+create a app where it shows the PSO particles movement where they reach the goal. ( PSO simulation)
+
+the particles are placed near each other randomly ( N particles ).
+the goal and other parameters will be entered by the user. ( any formula parameters, and goal position )
+
+user can set the animations speed so he/she can see the movement more clearly
+
+and save the particle positions on each iteration and give ability for the user to select them and place them on the UI
+
+give each particle a color or any thing else so that user can separate them from each other
+
+## Requirements:
+
+- main logic must be in Python
+- randomly place particles
+- the goal position and other parameters are entered by the user
+- the goal is in another color
+- animation speed ( will be set by the user )
+- the app should save each generations best solution with its fitness function result and user should have the ability to see them from a list and display them on the UI.
+- no library should be used for implementing PSO formula, you can use any language you want like ( php, node, c# ..etc )
+
+# 5. Random Chess Board challenge
+
+Place every piece of chess peaces randomly for both sides ( black and white ) using genetic algorithm
+
+## Rules:
+
+- white will move next
+- if any white pieces is under attack it must be defended by another piece
+- if any black pieces is under attack it must be defended by another piece
+- black king must not be checked or checkmated
+- white king must not be checkmated
+- each side has flexibility to have lost 0-5 pieces
+- maximum 1 king
+- maximum 1 queen
+- maximum 2 knights
+- maximum 2 rooks
+- maximum 2 bishops
+- maximum 8 pawns
+
+## Requirements:
+
+- main logic must be in Python
+- the app should save each generations best solution with its fitness function result and user should have the ability to see them from a list and display them on the board.
+- no library should be used for implementing KNN formula, you can use any language you want like ( php, node, c# ..etc )
+
+# 6. Magic Square Puzzle
+
+Solve a 3x3 Magic Square puzzle, where the sum of numbers in each row, column, and diagonal equals the same value, using a genetic algorithm.
+
+Search for magic square on the internet
+
+## Rules
+
+- allowed numbers are n > 0
+- no number duplication
+
+## Requirements:
+
+- main logic must be in Python
+- the app should save each generations best solution with its fitness function result and user should have the ability to see them from a list and display them on the board.
+- no library should be used for implementing KNN formula, you can use any language you want like ( php, node, c# ..etc )
